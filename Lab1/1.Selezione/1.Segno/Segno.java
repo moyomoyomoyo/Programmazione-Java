7691d63aba4bb600bb7c0b21fc6c14276e6aa4fb
Kay <124591761+moyomoyomoyo@users.noreply.github.com>
2023-10-23 13:45:57 +0200

    created class Segno

---
+++ Lab1/1.Selezione/1.Segno/Segno.java
@@ -0,0 +1,23 @@
+public class Segno {
+
+
+
+    public static void main(String[] args) {
+
+        Integer n = Integer.parseInt(args[0]);
+
+
+
+        if (n <= 0) {
+
+            System.out.println(n);
+
+        }else{
+
+            System.out.println("+" + n);
+
+        }
+
+    }
+
+}
