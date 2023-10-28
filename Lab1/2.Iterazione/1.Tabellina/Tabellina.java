f4a11f05294532750f3a50440415234cf249304e
Kay <124591761+moyomoyomoyo@users.noreply.github.com>
2023-10-28 15:14:40 +0200

    Delete Lab1 directory

--- Lab1/2.Iterazione/1.Tabellina/Tabellina.java
+++
@@ -1,9 +0,0 @@
-public class Tabellina {
-    public static void main(String[] args) {
-        Integer n = Integer.parseInt(args[0]);
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i + " x " + n + " = " + i*n);
-        }
-    }
-}
